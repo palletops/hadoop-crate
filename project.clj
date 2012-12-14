0bba32a88bff6ac625cbc2229d13493eaf20b235
Hugo Duncan <hugo@hugoduncan.org>
2012-12-14 15:22:06 -0500

    Update to non-snapshot dependencies

--- project.clj
+++ project.clj
@@ -3,15 +3,14 @@
   :url "http://github.com/palletops/hadoop-crate"
   :license {:name "All rights reserved"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.clojure/core.logic "0.8.0-beta3-SNAPSHOT"]
                  [org.clojure/data.xml "0.0.6"]
-                 [org.cloudhoist/pallet "0.8.0-SNAPSHOT"]
-                 [org.cloudhoist/java "0.8.0-SNAPSHOT"]
-                 [palletops/locos "0.1.0-SNAPSHOT"]
+                 [org.cloudhoist/pallet "0.8.0-alpha.6"]
+                 [org.cloudhoist/java "0.8.0-alpha.1"]
+                 [palletops/locos "0.1.0"]
                  [pathetic "0.4.0"]]
   :profiles {:dev
-             {:dependencies [[org.cloudhoist/pallet-vmfest "0.2.1-SNAPSHOT"]
-                             [org.cloudhoist/pallet "0.8.0-SNAPSHOT"
+             {:dependencies [[org.cloudhoist/pallet-vmfest "0.2.1"]
+                             [org.cloudhoist/pallet "0.8.0-alpha.6"
                               :classifier "tests"]
                              [ch.qos.logback/logback-classic "1.0.0"]]}}
   :test-selectors {:default (complement :live-test)
