989da0c2fa0fe56eca36510346304d51332d805d
Hugo Duncan <hugo@hugoduncan.org>
2013-01-07 12:21:38 -0500

    Update to pallet 0.8.0-alpha.7

--- project.clj
+++ project.clj
@@ -4,13 +4,13 @@
   :license {:name "All rights reserved"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/data.xml "0.0.6"]
-                 [org.cloudhoist/pallet "0.8.0-alpha.6"]
+                 [org.cloudhoist/pallet "0.8.0-alpha.7"]
                  [org.cloudhoist/java "0.8.0-alpha.1"]
                  [palletops/locos "0.1.0"]
                  [pathetic "0.4.0"]]
   :profiles {:dev
              {:dependencies [[org.cloudhoist/pallet-vmfest "0.2.1"]
-                             [org.cloudhoist/pallet "0.8.0-alpha.6"
+                             [org.cloudhoist/pallet "0.8.0-alpha.7"
                               :classifier "tests"]
                              [ch.qos.logback/logback-classic "1.0.0"]]}}
   :test-selectors {:default (complement :live-test)
