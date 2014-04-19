853e4a995903e6b3d77c9bed1d4a81b8ef4ed1e3
Antoni Batchelli <tbatchelli@acm.org>
2014-04-18 22:37:27 -0700

    Remove release code in prep for lein-pallet-release.

--- profiles.clj
+++ profiles.clj
@@ -4,10 +4,4 @@
                   :classifier "tests"]
                  [ch.qos.logback/logback-classic "1.0.0"]]}
  :no-checkouts {:checkout-deps-shares ^:replace []} ; disable checkouts
- :release
- {:plugins [[lein-set-version "0.2.1"]]
-  :set-version
-  {:updates [{:path "README.md"
-              :no-snapshot true
-              :search-regex
-              #"com.palletops/hadoop-crate \"\d+\.\d+\.\d+\""}]}}}
+ }
