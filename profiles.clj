04e39858424ce7515eeef5f5b6b908ee348b2640
Antoni Batchelli <tbatchelli@acm.org>
2014-04-18 22:35:46 -0700

    Update to pallet 0.8.0-RC.9, vmfest 0.4.0-alpha.1 and locos 0.1.1.

--- profiles.clj
+++ profiles.clj
@@ -1,6 +1,6 @@
 {:dev
- {:dependencies [[org.cloudhoist/pallet-vmfest "0.3.0-alpha.3"]
-                 [com.palletops/pallet "0.8.0-beta.8"
+ {:dependencies [[com.palletops/pallet-vmfest "0.4.0-alpha.1"]
+                 [com.palletops/pallet "0.8.0-RC.9"
                   :classifier "tests"]
                  [ch.qos.logback/logback-classic "1.0.0"]]}
  :no-checkouts {:checkout-deps-shares ^:replace []} ; disable checkouts
--- project.clj
+++ project.clj
@@ -7,9 +7,9 @@
   :license {:name "All rights reserved"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/data.xml "0.0.6"]
-                 [com.palletops/pallet "0.8.0-beta.8"]
-                 [com.palletops/java-crate "0.8.0-beta.4"]
-                 [com.palletops/locos "0.1.1-SNAPSHOT"]
+                 [com.palletops/pallet "0.8.0-RC.9"]
+                 [com.palletops/java-crate "0.8.0-beta.6"]
+                 [com.palletops/locos "0.1.1"]
                  [pathetic "0.4.0"]]
   :test-selectors {:default (complement :live-test)
                    :live-test :live-test
