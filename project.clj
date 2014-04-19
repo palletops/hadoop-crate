c9c36886e5f1d6e4b55d8c18cfeaed9fd775336b
Antoni Batchelli <tbatchelli@acm.org>
2014-04-18 22:28:21 -0700

    Release as EPL (was All Rights Reserved).

--- project.clj
+++ project.clj
@@ -1,6 +1,9 @@
 (defproject com.palletops/hadoop-crate "0.1.5-SNAPSHOT"
   :description "Crate for hadoop installation"
   :url "http://github.com/palletops/hadoop-crate"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :scm {:url "git@github.com:palletops/hadoop-crate.git"}
   :license {:name "All rights reserved"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/data.xml "0.0.6"]
